792c8864140acf1a1fd81a6bcd7f48389b7895ca
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2025-03-07 01:21:52 +0530

    automate chrome browser

---
+++ AliExpress/AliExpress/src/test/testFile.java
@@ -0,0 +1,8 @@
+public class testFile{
+    @Test
+    public void A(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.quite();
+        }
+        }
