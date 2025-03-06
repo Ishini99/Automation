e14941f5c00864788a5d647dbb1f8622e5a31ad1
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2025-03-07 01:37:49 +0530

    updated

--- AliExpress/AliExpress/src/test/java/testFile.java
+++ AliExpress/AliExpress/src/test/java/testFile.java
@@ -1,8 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class testFile{
     @Test
     public void A(){
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
-        driver.quite();
+        driver.quit();
         }
         }
