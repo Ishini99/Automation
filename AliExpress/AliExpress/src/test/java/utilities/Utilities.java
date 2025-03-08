cc006ef878e65de13ea8f3a16f0d7da4e472aec9
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2025-03-08 23:16:46 +0530

    updated utilities files

--- AliExpress/AliExpress/src/test/java/utilities/Utilities.java
+++ AliExpress/AliExpress/src/test/java/utilities/Utilities.java
@@ -1,4 +1,24 @@
 package utilities;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class Utilities {
+
+    protected BrowserFactory browserFactory;
+
+    @BeforeMethod
+    public void InitializeBrowser() {
+
+        browserFactory = BrowserFactory.getBrowserFactory();
+        browserFactory.getDriver().manage().window().maximize();
+        browserFactory.getDriver().get("https://www.daraz.lk/");
+    }
+
+    @AfterMethod
+    public void closeDriver(){
+
+        browserFactory.getDriver().quit();
+        browserFactory.threadLocal.remove();
+    }
 }
